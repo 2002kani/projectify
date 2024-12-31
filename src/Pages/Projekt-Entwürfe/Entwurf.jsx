396fbadf32d8f9448cca383c3aca6b836095b3a3
02Kani <kanikaradag02@gmail.com>
2024-12-31 14:24:44 +0100

    #

--- src/Pages/Projekt-Entwürfe/Entwurf.jsx
+++ src/Pages/Projekt-Entwürfe/Entwurf.jsx
@@ -3,10 +3,16 @@ import Titelkarte from "../../Components/Titelkarte/Titelkarte";
 import "./Entwurf.css"
 
 const Entwurf = () => {
+
     return(
-        <div className="entwurf">
-            
-            <Titelkarte titel={"Projekt Entwürfe"}/>
+        <div className="entwürfe-kein-entwurf">
+            <div className="eingabe-feld">
+                <div className="information">
+                    <h1> Noch kein Entwurf erstellt? </h1>
+                    <p> Lass deine Projekte Wirklichkeit werden, indem du jetzt deinen Entwurf planst und umsetzt!</p>
+                </div>
+                <button className="entwurf-erstellen"> + </button>
+            </div>
         </div>
     );
 }
