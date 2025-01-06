d4759dbcff5214e18b2275b65590c6ca8eaf2702
02Kani <kanikaradag02@gmail.com>
2025-01-06 22:46:18 +0100

    #

--- src/Pages/Suche/Suche.jsx
+++ src/Pages/Suche/Suche.jsx
@@ -1,14 +1,30 @@
-
+import { useEffect, useState } from "react";
 import "./Suche.css"
 
+
 const Suche = () => {
+
+    const [alleIdeen, setAlleIdeen] = useState([]);
+    const [alleEntwürfe, setAlleEntwürfe] = useState([]);
+
+
+
     return(
         <div className="suche">
-            <h1> Suche nach deinen Projekten </h1>
             <div className="suchleiste">
-                <input type="text" placeholder="Projektname.." id="suchleiste-input" maxLength={40}/>
+                <input type="text" placeholder="Titel des Projekts..." id="suchleiste-input" maxLength={40}/>
                 <i className='bx bx-search-alt' id="suchleiste-btn" ></i>
             </div>
+
+            <div className="suchanzeigen">
+                <div className="ideen-reihe">
+                    
+                </div>
+                <div className="entwürfe-reihe">
+                    
+                </div>
+            </div>
+
         </div>
     );
 }
