ab591c13def2a8a1c4ed21b768b2f3cdc2cd4556
02Kani <kanikaradag02@gmail.com>
2025-01-06 23:01:43 +0100

    #

--- src/Pages/Suche/Suche.jsx
+++ src/Pages/Suche/Suche.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./Suche.css"
+import axios from "axios";
 
 
 const Suche = () => {
@@ -8,6 +9,11 @@ const Suche = () => {
     const [alleEntwürfe, setAlleEntwürfe] = useState([]);
 
 
+    useEffect(()=>{
+        axios.get("http://localhost:5001/ideen")
+            .then((response) => setAlleIdeen(response.data))
+            .catch((err) => console.log("Fehler beim Abrufen der Ideen: ", err));
+    },[]);
 
     return(
         <div className="suche">
@@ -18,7 +24,15 @@ const Suche = () => {
 
             <div className="suchanzeigen">
                 <div className="ideen-reihe">
-                    
+                    {alleIdeen.map((idee, index)=>(
+                        <div key={index} className="idee-karte">
+                        <div className="ideen-top">
+                            
+                        </div>
+                        <h2>{idee.titel}</h2>
+                        <p>{idee.beschreibung}</p>
+                    </div>
+                    ))}
                 </div>
                 <div className="entwürfe-reihe">
                     
