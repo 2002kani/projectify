bdc477b4a1a54dd3f29b4fbc09d2332e9731fc71
02Kani <kanikaradag02@gmail.com>
2025-01-17 16:05:42 +0100

    #

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,23 +1,41 @@
-import "./Login.css"
 import { useState } from "react";
-import { useForm } from "react-hook-form"
+import "./Login.css"
+import { login, signup } from "../../firebase";
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm();
-    const [data, setData] = useState("");
+    const [signState, setSignState] = useState("Anmelden");
+    const [formData, setFormData] = useState({
+
+    })
+
+    const user_auth = async(e)=>{
+        e.preventDefault();
+        setLoading(true);
+        if(signState=== "Anmelden"){
+            await login(email, password);
+        } else{
+            await signup(name, email, password);
+        }
+        setLoading(false);
+    }
 
     return(
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))} className="login">
-            <div className="login-top">
-                <h2> Ganz einfach Registrieren </h2>
-                <p> um Projectify zu benutzen und deine Projekte effizient zu planen </p>
+        <div className="signup">
+            <h2> Ganz einfach Registrieren </h2>
+            <p> Registriere dich um effizient deine eigenen Projekte zu planen und bearbeiten. </p>
+            <div className="input-field">
+                <label> Name </label>
+                <input type="text" maxLength={30} required/>
+                <label> E-mail </label>
+                <input type="email" required/>
+                <label> Passwort </label>
+                <input type="password" required/>
+                <button> Registrieren </button>
             </div>
-            <div className="login-form">
-                <input {...register("firstName")} placeholder=""></input>
-            </div>
-        </form>
+            <p className="change-option">Schon Registriert? <span>Anmelden</span></p>
+        </div>
     );
-}
+} 
 
 export default Login
