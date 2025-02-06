06c525ee0de4b285ec2bad2e0f808c81ef4c1488
02Kani <kanikaradag02@gmail.com>
2025-02-06 14:23:21 +0100

    #

--- src/Pages/Konversationen/Konversation.jsx
+++ src/Pages/Konversationen/Konversation.jsx
@@ -1,6 +1,8 @@
 import Titelkarte from "../../Components/Titelkarte/Titelkarte";
 import Sidebar from "../../Components/Sidebar/sidebar";
 
+const chatBotAPI = import.meta.env.VITE_API_KEY_CB;
+
 const Konversation = () => {
     return(
         <div className="konversation">
